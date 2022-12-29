2b15bf26ea826ac8b214da9d11eec566e0c11618
nkeskin <keskin.nmn@gmail.com>
2022-12-29 22:38:52 +0300

    add thymeleaf, add home page, home controller and add a test for home controller

---
+++ src/test/java/com/example/demo/controller/HomeControllerTest.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class HomeControllerTest {
+
+    HomeController homeController = new HomeController();
+
+    @Test
+    void testHome() {
+        String result = homeController.home();
+        Assertions.assertEquals("home", result);
+    }
+}
