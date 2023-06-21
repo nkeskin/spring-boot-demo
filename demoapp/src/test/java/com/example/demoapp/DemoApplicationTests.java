e18292b453739e08af3145d51452035f915c50e6
nkeskin <keskin.nmn@gmail.com>
2023-06-21 22:32:27 +0300

    update project structure and upgrade spring boot version add eureka server - client capabilities

---
+++ demoapp/src/test/java/com/example/demoapp/DemoApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+}
